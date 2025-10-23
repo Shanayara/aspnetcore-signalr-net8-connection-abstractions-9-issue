4377dd2b9259d55b0ea35bcb221c6962aba2ad5f
Shanayara <75668666+Shanayara@users.noreply.github.com>
2025-10-23 15:16:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+using WebApplication1.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
